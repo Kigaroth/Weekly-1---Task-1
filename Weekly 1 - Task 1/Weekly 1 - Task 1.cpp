c8bca3bf4e71906ce8c79db3e392b1334265d12e
Kigaroth <hp_20@hotmail.com>
2020-09-05 20:03:43 +0200

    Fixed using "std::string" instead of "std::cin".

--- Weekly 1 - Task 1/Weekly 1 - Task 1.cpp
+++ Weekly 1 - Task 1/Weekly 1 - Task 1.cpp
@@ -1,9 +1,42 @@
 // Weekly 1 - Task 1.cpp: //
 
 #include <iostream>
+#include <string>
 
-
+std::string full_name = "";
+std::string Initial ="";
+std::string date_of_birth = "";
+std::string age = "";
+std::string phone_number = "";
 
 int main() {
+	std::cout << "Hello!\n\n";
+	std::cout << "Please enter your full name:  ";
+	std::getline(std::cin, full_name);
+	std::cout << "Thank you. \n\n";
 
+	
+	std::cout << "Please enter the initial of your name:  ";
+	std::getline(std::cin, Initial);
+	std::cout << "Thank you. \n\n";
+
+
+	std::cout << "Please enter your age:  ";
+	std::getline(std::cin, age);
+	std::cout << "Thank you. \n\n";
+
+	std::cout << "Please enter your date of birth:  ";
+	std::getline(std::cin, date_of_birth);
+	std::cout << "Thank you. \n\n";
+
+	std::cout << "Please enter your phone number:  ";
+	std::getline(std::cin, phone_number);
+	std::cout << "Thank you. \n\n";
+	
+	system("cls");
+	std::cout << "Here is your info: \n\n" "\t Name| " << full_name;
+	std::cout << "\n\t Initial| " << Initial;
+	std::cout << "\n\t Age| " << age;
+	std::cout << "\n\t Phone number| " << phone_number;
+	std::cout << "\n\t Birthday| " << date_of_birth;
 }
